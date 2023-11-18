1b11122a7078bab13a868bafea9c2034d5a200a4
오하늘 <dhgksmfdl@naver.com>
2023-11-18 16:53:24 +0900

    11.18
    오타수정, 구경이지만 혹시모르니 메모..

--- Assets/01Script/JumpCount.cs
+++ Assets/01Script/JumpCount.cs
@@ -11,7 +11,7 @@ public class JumpCount : MonoBehaviour
 
     //점프 count 하나씩 늘어날 때마다 아이콘 하나씩 사라지게(투명하게)
 
-    public void JumpIconUpdate(int JumpCount) // 화면에 표시되어야하는 점프 icon갯수를 전달 받아서 표시
+    public void JumpIconUpdate(int JumpCount) // 화면에 표시되어야하는 점프 icon 개수를 전달 받아서 표시
     {
         for(int i = 0; i < 4; i++)
         {
